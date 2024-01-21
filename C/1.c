dc9a3d87c4078c69c11a37e2760b11b178ef6ce9
shailendra <shailendrabhosale2003@gmail.com>
2024-01-21 19:37:26 +0530

    modified

---
+++ C/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Addition off 2 numbers\n\n");
+	int a, b;
+	printf("Enter two numbers: ");
+	scanf("%d %d", &a, &b);
+	printf("\nResult: %d\n", a+b);
+	return 0;
+}
--- Python/helo_wrld.py
+++ Python/helo_wrld.py
@@ -1,5 +1,1 @@
-a=int(input("Enter your number: "))
-print("")
-print("Your table: ")
-for i in range(1,11):
-    print(a*i)
+print("Hello World!!!")
